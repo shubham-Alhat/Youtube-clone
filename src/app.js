a6cb146a5290ba5f11f0ddd3c0dfcbf479a0ed38
shubham-Alhat <shubhamalhat520@gmail.com>
2025-06-18 14:14:12 +0530

    assignment setup complete

--- src/app.js
+++ src/app.js
@@ -19,11 +19,27 @@ app.use(express.static("public")); // to store temp files on server. such files 
 
 app.use(cookieParser()); // allow express to set and read client's browser cookies.
 
-// routes import
+//routes import
 import userRouter from "./routes/user.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
-// routes declaration
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 // Here our url looks like, https://localhost:8000/api/v1/users/register or login.
 // when `api/v1/users` hit the browser, the control goes to user.routes.js. there when hit register then register methods
